d5e166bb691073b2399fe411a7e31a84a5cecff3
Suven-p <pandeysuven@gmail.com>
2023-02-26 19:09:02 +0545

    Add custom application

---
+++ ZirconProject/custom_application/application.py
@@ -0,0 +1,19 @@
+from dm_control.viewer import application
+from dm_control.viewer import user_input
+
+_INSERT_TARGET = (user_input.MOUSE_BUTTON_LEFT, user_input.MOD_SHIFT)
+_SELECT_OBJECT = user_input.DoubleClick(user_input.MOUSE_BUTTON_LEFT)
+
+
+class Application(application.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _perform_deferred_reload(self, *args, **kwargs):
+        super()._perform_deferred_reload(*args, **kwargs)
+
+        def custom_handler():
+            body_id, position = self._viewer.camera.raycast(
+                self._viewport, self._viewer._mouse.position)
+            return print(position)
+        self._viewer._input_map.bind(custom_handler, _INSERT_TARGET)
